055d17b47963c13c3226f4851c8bd4caec3e0dfd
Jason Wu <jazwu@ucdavis.edu>
2022-05-24 10:24:55 -0700

    init commit

---
+++ DupFinder.py
@@ -0,0 +1,12 @@
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = "playlist-modify-private"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
